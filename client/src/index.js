c3a11b42a43d6c4d80b041dc3316f2a608978830
Adrian Humphreys <adrian.humphreys@silverstripe.com>
2020-07-08 07:22:46 +1200

    fonts and builds

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App'
-import './styles/app.scss'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./styles/app.scss";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
--- templates/Telescope.ss
+++ templates/Telescope.ss
@@ -1,3 +1,4 @@
 <div id="root"></div>
 <script src="$resourceURL("adrhumphreys/telescope:client/dist/index.js")" async></script>
 <link rel="stylesheet" href="$resourceURL("adrhumphreys/telescope:client/dist/index.css")" />
+<link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
